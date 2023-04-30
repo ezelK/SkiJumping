6e73de13899a9fe1773849068447416b1189c8fe
rbozburun <resul.bozburun00@gmail.com>
2023-04-30 18:57:57 +0300

    ExitButton aded

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class UIMainMenu : MonoBehaviour
 {
     [SerializeField] Button _playBtn;
+    [SerializeField] Button _exitBtn;
 
     // Start is called before the first frame update
     void Start()
     {
         _playBtn.onClick.AddListener(StartNewGame);
+        _exitBtn.onClick.AddListener(ExitGame);
     }
 
     private void StartNewGame()
@@ -19,4 +21,9 @@ public class UIMainMenu : MonoBehaviour
         ScenesManager.Instance.LoadNewGame();
     }
 
+    private void ExitGame()
+    {
+        // Quit the app
+        Application.Quit();
+    }
 }
